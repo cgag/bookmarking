403da3dbbd635619bdce4795b760c6e441f8208e
Curtis Gagliardi <gagliardi.curtis@gmail.com>
2013-01-30 00:20:10 -0600

    Improve boilerpipe wrapper:
    _    Use a common user-agent and accept cookies to avoid
         common anti-scraping measures
    -    Use a protocol to handle special case for URL type
    -    Wrap paragraphs in <p> tags
    
    Still need to separate out the view related functions

--- src/bookmarking/models/boilerpipe.clj
+++ src/bookmarking/models/boilerpipe.clj
@@ -3,14 +3,49 @@
             [hiccup.core :refer :all]
             [hiccup.form :refer :all]
             [clojure.string :as s])
-  (:import de.l3s.boilerpipe.extractors.ArticleExtractor))
+  (:import de.l3s.boilerpipe.extractors.ArticleExtractor
+           de.l3s.boilerpipe.extractors.DefaultExtractor))
 
-(def ^:private test-url "http://www.flightglobal.com/news/articles/elon-musk-boeing-787-battery-fundamentally-unsafe-381627/")
+(declare to-html)
+
+(def ^:private fake-agent "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)")
 
 (def article-extractor (ArticleExtractor/getInstance))
+(def default-extractor (DefaultExtractor/getInstance))
 
-(defn get-text [url]
-  (.getText article-extractor (java.net.URL. url)))
+(defprotocol TextExtractor
+  "Extract the text from a given source"
+  (get-text-p [source extractor]
+    "Get text from a given source using "))
+
+(extend-protocol TextExtractor
+  java.net.URL
+  (get-text-p [source extractor]
+    (let [cookie-store (clj-http.cookies/cookie-store)
+          resp    (clj-http.client/get (str source) {:cookie-store cookie-store
+                                                     :headers {"User-Agent" fake-agent}})
+          body-str (:body resp)]
+      (get-text-p body-str extractor)))
+  Object
+  (get-text-p [source extractor]
+    (.getText extractor source)))
+
+(defn get-text
+  "text-source can be a java.net.URL, a String, a Reader, etc
+   TODO: Checkout boilerpipe source and figure out what else"
+  [source & [extractor]]
+  (let [extractor (or extractor article-extractor)]
+    (get-text-p source extractor)))
+
+(defn get-html [source & [extractor]]
+  (let [text (get-text source extractor)]
+    (to-html text)))
+
+(defn to-html [text]
+  (let [paragraphs (s/split text #"\n")]
+    (->> paragraphs
+         (map #(str "<p>" % "</p>"))
+         (s/join ""))))
 
 (declare boilerpipe-form)
 
@@ -21,7 +56,8 @@
 (defn boilerpipe-form []
   (form-to [:post "/boilerpipe"]
            (label "url" "url: ")
-           (text-field "url")))
+           (text-field "url")
+           (submit-button "Get Article")))
 
 (defn handle-post [url]
-  (s/replace (get-text url) #"\n" "<br />"))
+  (get-html (java.net.URL. url)))
